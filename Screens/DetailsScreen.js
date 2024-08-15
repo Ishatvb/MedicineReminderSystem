6e7623166cd05a0d5c38e9409bfc3ee0b62c3553
Ishatvb <Ishatvbeohar22082003@gmail.com>
2024-08-15 21:42:14 +0530

    Bottom Navigation added

---
+++ Screens/DetailsScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Button, Text, View} from 'react-native'
+function DetailsScreen(){
+    return(
+        <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+            <Text style={{color:"black",fontSize:22}}>Details Screen</Text>
+        </View>
+    );
+}
+
+export default DetailsScreen;
